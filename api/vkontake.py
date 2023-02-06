f18a4347f7dca122d9dd52ff4e0f96ed43cbdd1d
IlyaAriskin <121175171+IlyaAriskin@users.noreply.github.com>
2023-02-06 11:47:57 +0300

    fix

--- api/vkontake.py
+++ api/vkontake.py
@@ -6,29 +6,53 @@ from vk_api import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from config.keyboard import keyboard
-from config.settings import user_token
 from users.user import User
 
 
 class VkAPI:
+
+    def __init__(
+            self,
+            user_token: str,
+            group_token: str,
+            count: int = 10,
+    ):
+        self.user_api = vk_api.VkApi(token=user_token)
+        self.group_api = vk_api.VkApi(token=group_token)
+        self.longpoll = VkLongPoll(self.group_api)
+        self.count = count
+
+    def user_search_generator(self, request_data: dict):
+        request_data.update({"count": self.count})
+        request_data.update({"offset": 0})
+
+        while True:
+            result = self.user_api.method("users.search", request_data)
+
+            # проверка результата
+            if not (items := result.get('items')):
+                break
+            for item in items:
+                yield item
+
+            request_data['offset'] += self.count
+
     # Запрашиваем пол
-    @staticmethod
-    def get_user_sex(user: User, vk):
-        result = vk.method("users.get", {"user_id": user.id, "fields": {"sex"}})[0]
+    def get_user_sex(self, user: User):
+        result = self.group_api.method("users.get", {"user_id": user.id, "fields": {"sex"}})[0]
         if 'sex' in result and result['sex'] != 0:
             return int(result['sex'])
         # запрашиваем пол
         question = "пол"
-        result = VkAPI.ask_question(user, question, vk)
+        result = self.ask_question(user, question)
         if result == "м" or result == "мужской" or result == "муж":
             return 2
         else:
             return 1
 
     # Проверяем, есть ли от пользователя новое сообщение и возвращаем текст сообщения
-    @staticmethod
-    def wait_for_answer_from_user(longpoll) -> dict:
-        for event in longpoll.listen():
+    def wait_for_answer_from_user(self) -> dict:
+        for event in self.longpoll.listen():
             if event.type != VkEventType.MESSAGE_NEW or not event.to_me:
                 continue
             # получаем введеный пользователем текст
@@ -38,27 +62,23 @@ class VkAPI:
             }
 
     # Вывод сообщения пользователю
-    @staticmethod
-    def write_msg(user: User, message, vk):
-        vk.method('messages.send', {'user_id': user.id, 'message': message, 'keyboard': keyboard, 'random_id': randrange(10 ** 7), })
+    def write_msg(self, user: User, message):
+        self.group_api.method('messages.send', {'user_id': user.id, 'message': message, 'keyboard': keyboard,
+                                                'random_id': randrange(10 ** 7), })
 
     # Просим ввести пользователя дополнительную информацию, если она отсутствовала:
-    @staticmethod
-    def ask_question(user: User, question, vk) -> str:
-        longpoll = VkLongPoll(vk)
+    def ask_question(self, user: User, question) -> str:
         question_text = f"Пожалуйста, введите ваш {question}: "
         # Спрашиваем пользователя вопрос
-        VkAPI.write_msg(user, question_text, vk)
+        self.write_msg(user, question_text)
         # Читаем ответ
-        answer_of_user = VkAPI.wait_for_answer_from_user(longpoll)['text']
+        answer_of_user = self.wait_for_answer_from_user()['text']
         return answer_of_user.strip().lower()
 
     # Достаем айди города по названию
-    @staticmethod
-    def get_city_id_by_title(title: str) -> int:
-        vk_api_with_person_token = vk_api.VkApi(token=user_token)
+    def get_city_id_by_title(self, title: str) -> int:
         city_id = 0
-        result = vk_api_with_person_token.method("database.getCities", {
+        result = self.user_api.method("database.getCities", {
             "country_id": 1,
             "q": f'{title}',
             "need_all": 0,
@@ -70,10 +90,9 @@ class VkAPI:
         return city_id
 
     # Запрашиваем город
-    @staticmethod
-    def get_user_city(user: User, vk) -> dict:
+    def get_user_city(self, user: User) -> dict:
         city = {}
-        result = vk.method("users.get", {"user_id": user.id, "fields": {"city"}})[0]
+        result = self.group_api.method("users.get", {"user_id": user.id, "fields": {"city"}})[0]
         if 'city' in result:
             city = result['city']
         else:
@@ -81,14 +100,14 @@ class VkAPI:
             # просим ввести название города
             city['id'] = 0
             while not city['id']:
-                city_title = VkAPI.ask_question(user, question, vk)
+                city_title = self.ask_question(user, question)
                 city['title'] = city_title
                 # достаем id города
-                city['id'] = VkAPI.get_city_id_by_title(city_title)
+                city['id'] = self.get_city_id_by_title(city_title)
                 if not city['id']:
                     text = "Вы ввели несуществующий город. Попробуйте еще раз."
                     time.sleep(1.5)
-                    VkAPI.write_msg(user, text, vk)
+                    self.write_msg(user, text)
 
         return city
 
@@ -101,21 +120,20 @@ class VkAPI:
         return age
 
     # Запрашиваем возраст
-    @staticmethod
-    def get_user_age(user: User, vk):
-        result = vk.method("users.get", {"user_id": user.id, "fields": {"bdate"}})[0]
+    def get_user_age(self, user: User):
+        result = self.group_api.method("users.get", {"user_id": user.id, "fields": {"bdate"}})[0]
         if 'bdate' in result and len(result['bdate']) > 5:
             age = VkAPI.get_user_age_from_date(date_str=result['bdate'])
             return age
         else:
             result = ""
             while not result.isdigit():
-                result = VkAPI.ask_question(user, "возраст", vk)
+                result = self.ask_question(user, "возраст")
             return result
 
     # Получить фотографию людей для знакомства
-    @staticmethod
-    def get_photos_of_person(person_id, vk_api_user_token):
+
+    def get_photos_of_person(self, person_id):
         request = {
             "type": "album",
             "owner_id": person_id,
@@ -123,7 +141,7 @@ class VkAPI:
             "count": 25,
             "skip_hidden": 1
         }
-        photos = vk_api_user_token.method("photos.getAll", request)['items']
+        photos = self.user_api.method("photos.getAll", request)['items']
         # Сортировка по количеству лайков
         photos = sorted(photos, key=lambda d: d['likes']['count'], reverse=True)
         return photos
